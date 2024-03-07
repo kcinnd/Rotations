defde8a32e956a84d3039543e4fae915670d76b1
kcinnd <152316054+kcinnd@users.noreply.github.com>
2024-03-07 07:24:28 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.querySelectorAll('.grid-item').forEach(item => {
   item.addEventListener('click', () => {
+    // Rotate the image
     const currentRotation = item.style.transform.match(/rotateY\((\d+)deg\)/);
     const currentAngle = currentRotation ? parseInt(currentRotation[1]) : 0;
     const newRotation = `rotateY(${currentAngle + 90}deg)`;
@@ -7,5 +8,10 @@ document.querySelectorAll('.grid-item').forEach(item => {
     setTimeout(() => {
       item.style.transform = newRotation + ' scale(1)';
     }, 150);
+
+    // Play click sound
+    const clickSound = document.getElementById('clickSound');
+    clickSound.currentTime = 0; // Rewind to the start
+    clickSound.play();
   });
 });
