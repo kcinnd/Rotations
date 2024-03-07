a45ff0dbc1683ff58077c48624b96ddf0a8142eb
kcinnd <152316054+kcinnd@users.noreply.github.com>
2024-03-07 04:46:34 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
+    openPuzzle('Puzzle3x3', document.getElementsByClassName('tablink')[0]);
+    
     const clickSound = new Audio('sounds/click-buttons.mp3'); 
     // Function to rotate a puzzle piece
     function rotatePiece(piece) {
@@ -23,3 +25,21 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+function openPuzzle(puzzleId, element) {
+    // Hide all tab content by default
+    var tabcontent = document.getElementsByClassName("tabcontent");
+    for (var i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Remove the "active" class from all tab links
+    var tablinks = document.getElementsByClassName("tablink");
+    for (var i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab content and add "active" class to the clicked tab link
+    document.getElementById(puzzleId).style.display = "block";
+    element.className += " active";
+}
